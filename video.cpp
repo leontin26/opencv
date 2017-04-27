d5c5fd68b8cab1950fb1359f78afbaed8a50b53e
leontin26 <leontin26@yahoo.com>
2017-04-27 19:36:07 +0300

    camera video

---
+++ video.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <iostream>
+
+#include "opencv2\core\mat.hpp"
+#include "opencv2\opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main(void)
+{
+	Mat a;
+
+	VideoCapture video(0);
+	
+	if (!video.isOpened()) {
+		printf("cannot connect to camera !!!");
+		getchar();
+		return 1;
+	}
+
+	namedWindow("camera");
+	
+	while (1) {
+
+		video >> a;
+		imshow("camera", a);
+		if (waitKey(1) == 27) 
+			break;
+	}
+	
+}
